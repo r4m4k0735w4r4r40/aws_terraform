a16a2bae5bd97e8fd0d02df37d00ff00b299977d
Ramakoteswara Rao Mareedu <mareedu@achala.local>
2022-08-02 16:02:39 +0530

    Unit test added

--- test_sign_in.py
+++ test_sign_in.py
@@ -5,7 +5,7 @@ from lambdas import sign_in
 
 @mock_dynamodb
 def test_signup_lambda():
-    ddb_client = boto3.resource('dynamodb')
+    ddb_client = boto3.resource('dynamodb',region_name="ap-south-1")
     table_name = 'users_data'
     table = ddb_client.create_table(TableName=table_name,
                                     KeySchema=[{'AttributeName': 'user_name','KeyType': 'HASH'}],
